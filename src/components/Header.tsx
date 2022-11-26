1c91f829958c83a86539286dfb081d909d76b578
Taiwo Adewale <adewaletaiwo08@gmail.com>
2022-11-26 09:49:14 +0100

    refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { FiSearch } from "react-icons/fi";
+import { HiMenuAlt1 } from "react-icons/hi";
 import { useDispatch } from "react-redux";
 
 import { Navbar, SocialIcons } from ".";
-import { setIsSearch } from "../redux/features/stateSlice";
+import { setIsNavMobile, setIsSearch } from "../redux/features/stateSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
 
   return (
     <div className="container">
-      <div className="flex flex-col xl:gap-y-10 pt-16">
-        <div className="flex items-center py-2 justify-center">
-          <div className="w-1/3">
+      <div className="flex flex-col xl:gap-y-10 pt-2 md:pt-6 xl:pt-16">
+        <div className="flex flex-wrap items-center py-2 justify-between md:justify-center">
+          <div className="md:w-1/3">
             <div className="hidden xl:block">
               <SocialIcons />
             </div>
 
-            <div className="xl:hidden"></div>
+            <button
+              onClick={() => dispatch(setIsNavMobile(true))}
+              className="xl:hidden cursor-pointer xs:p-1 p-2"
+            >
+              <HiMenuAlt1 className="xs:h-7 xs:w-7 w-[30px] h-[30px] fill-btnPrimary" />
+            </button>
           </div>
 
           <a
             href="#"
-            className="w-1/3 text-center text-6xl text-heading font-bold"
+            className="md:w-1/3 text-center xs:text-[35px] text-[40px] md:text-[50px] xl:text-6xl text-heading font-bold transition-all duration-300"
           >
             Blueway
           </a>
-          <div className="w-1/3">
+          <div className="md:w-1/3">
             <button
               onClick={() => dispatch(setIsSearch(true))}
-              className="w-10 h-10 bg-primary rounded-full flex justify-center items-center ml-auto"
+              className="xs:w-8 xs:h-8 w-10 h-10 bg-primary rounded-full flex justify-center items-center ml-auto"
             >
-              <FiSearch className="stroke-white w-[18px] h-[18px]" />
+              <FiSearch className="stroke-white w-[18px] xs:h-4 h-[18px]" />
             </button>
           </div>
         </div>
