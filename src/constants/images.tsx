f61d357271242f6fc2cb8c7e69459c96d378562b
Taiwo Adewale <adewaletaiwo08@gmail.com>
2022-11-26 12:21:34 +0100

    setup images

---
+++ src/constants/images.tsx
@@ -0,0 +1,9 @@
+import mountain01 from "../assets/mountain-1.jpg";
+import mountain02 from "../assets/mountain-2.jpg";
+import person01 from "../assets/person-1.jpeg";
+
+export default {
+  mountain01,
+  mountain02,
+  person01,
+};
