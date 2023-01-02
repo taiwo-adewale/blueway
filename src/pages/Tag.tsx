56fa029941c643169146efb988eb9f7710983aac
Taiwo Adewale <adewaletaiwo08@gmail.com>
2023-01-02 20:02:46 +0100

    setup page structure of tags page

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { Newsletter, Sidebar } from "../components";
+import HeaderFooterWrap from "../wrapper/HeaderFooterWrap";
+
 const Tag = () => {
-  return <div>Tag</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="flex w-full gap-x-12 pt-4 md:pt-8 xl:pt-12 pb-12">
+          <div className="flex-grow"></div>
+          <Sidebar />
+        </div>
+      </div>
+      <Newsletter />
+    </>
+  );
 };
 
-export default Tag;
+export default HeaderFooterWrap(Tag);
